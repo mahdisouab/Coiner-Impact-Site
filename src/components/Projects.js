660bf30f8df1799fb28c72f6634f7cd5b5145c04
mahdisouab <97970543+mahdisouab@users.noreply.github.com>
2022-09-10 23:51:40 +0100

    project to push

---
+++ src/components/Projects.js
@@ -0,0 +1,79 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.jpg";
+import projImg3 from "../assets/img/project-img3.jpg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+
+    {
+      title: "Business Startup",
+      description: "Ethereum merge coming on September",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Metaverse: the dangerous path",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Crypto prices reach an all time high",
+      imgUrl: projImg3,
+    },
+    
+  ];
+  
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Featured Articles</h2>
+                  <p></p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <h2>Latest Articles</h2>
+                      <Row>
+                        {projects.map((project, index) => {
+                          return <ProjectCard key={index} {...project} />;
+                        })}
+                      </Row>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+  
+}
